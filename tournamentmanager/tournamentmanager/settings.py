94a5ad5b2325fa1f3d41bacb90ba7d58875a6ae2
EduardoCuestaC <A00570171@itesm.mx>
2019-11-25 15:47:59 -0600

    users, tournamnets

--- tournamentmanager/tournamentmanager/settings.py
+++ tournamentmanager/tournamentmanager/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users.apps.UsersConfig',
+    'tournaments.apps.TournamentsConfig',
 ]
 
 MIDDLEWARE = [
@@ -74,10 +76,14 @@ WSGI_APPLICATION = 'tournamentmanager.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tournamentmanager',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+     }
 }
 
 
@@ -118,3 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'users.User'
--- tournamentmanager/tournaments/models.py
+++ tournamentmanager/tournaments/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+
+class Tournament(models.Model):
+    name = models.CharField(null=False, max_length=100)
+    description = models.TextField(max_length=500)
+    date = models.DateField(null=False)
+    users = models.ManyToManyField(settings.AUTH_USER_MODEL)
