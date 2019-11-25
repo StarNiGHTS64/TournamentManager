c180a2ae0d948fd2e69c5a2718f2e02ce1eb54aa
EduardoCuestaC <A00570171@itesm.mx>
2019-11-25 16:06:55 -0600

    add decorator

--- tournamentmanager/tournaments/views.py
+++ tournamentmanager/tournaments/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Tournament
 
 # Create your views here.
+
+
+class TournamentListView(ListView):
+    model = Tournament
+    paginate_by = 100
--- tournamentmanager/users/models.py
+++ tournamentmanager/users/models.py
@@ -5,4 +5,8 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    name = models.TextField(max_length=500)
+    email = models.TextField(max_length=500)
+    is_admin = models.BooleanField(default=False)
+    is_coach = models.BooleanField(default=False)
+    is_anon = models.BooleanField(default=True)
